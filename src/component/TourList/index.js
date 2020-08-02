6806e5953d9bc0e1f0b6ab92b842322595485abf
orxan196 <orkhanmq@code.edu.az>
2020-08-02 08:53:45 +0400

    first commit

---
+++ src/component/TourList/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Tour from ".././tour"
+import './tourlist.scss'
+import {tourData} from "../../tourData"
+
+export default class index extends Component {
+    state={
+        tours: tourData
+    }
+    removeTour = id =>{
+        const{tours} =this.state
+        const sortedTours = tours.filter(tour =>tour.id !==id);
+        this.setState({
+            tours:sortedTours
+        })
+    }
+    render() {
+       // console.log(this.state.tours)
+       const {tours} =this.state;
+        return <section className="tourlist">
+            {
+                tours.map(tour =>(<Tour key={tour.id} tour={tour} removeTour={this.removeTour} ></Tour>))
+            }
+        </section>;
+    }
+}
